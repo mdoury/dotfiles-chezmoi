439d5da4518d94406c8dac054ffafccd86083855
Maxime Doury <maxime.doury@gmail.com>
2022-12-28 03:07:22 +0100

    Improve neogit style a little

--- dot_config/nvim/lua/config/neogit.lua
+++ dot_config/nvim/lua/config/neogit.lua
@@ -1,1 +1,11 @@
-Safe('neogit').setup { disable_commit_confirmation = true, disable_signs = true, disable_builtin_notifications = true }
+Safe('neogit').setup({
+    disable_commit_confirmation = true,
+    disable_builtin_notifications = true,
+    signs = {
+        section = {"", ""},
+        item = {'', ''}
+    },
+    integrations = {
+      -- diffview = true
+    }
+})
