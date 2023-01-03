58a24da824e083a33c76d21eee4ccabd0e88ded8
Maxime Doury <maxime.doury@gmail.com>
2023-01-03 18:21:40 +0100

    Install treesitter compatible theme

--- dot_config/nvim/lua/config/colorscheme.lua
+++ dot_config/nvim/lua/config/colorscheme.lua
@@ -1,4 +1,13 @@
 -- Colorscheme
 vim.opt.termguicolors = true
 vim.opt.background = 'dark'
-vim.cmd [[colorscheme nord]]
+
+vim.g.nord_contrast = true
+vim.g.nord_borders = true
+vim.g.nord_disable_background = false
+vim.g.nord_italic = false
+vim.g.nord_uniform_diff_background = true
+vim.g.nord_bold = false
+
+-- Load the colorscheme
+require('nord').set()
